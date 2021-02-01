089a6559e99c40fdc5e567b803b8180528c9bfb3
prasaanth11 <77889861+prasaanth11@users.noreply.github.com>
2021-02-01 20:05:36 +0530

    Create 1.py

---
+++ PS-2/1.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@author: prasaanth s
+"""
+
+for i in range(1,101):
+    
+    if i%3==0 and i%5!=0:
+        print('Fizz')
+    
+    elif i%3 != 0 and i%5==0:
+        print('Buzz')
+    
+    elif i%5 == 0 and i%3 == 0:
+        print('FizzBuzz')
+    
+    else:
+        print(i)
+    
